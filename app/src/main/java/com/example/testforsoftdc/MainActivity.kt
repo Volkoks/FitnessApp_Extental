c0bdb724001d7d2345169132023c990c13cfc0d3
Alex Volkov <volk2390@gmail.com>
2021-03-23 21:09:57 +0300

    Переделал кнопки с MaterialButton на TextView
    Вынес код настороек View обратно во view из style

--- app/src/main/java/com/example/testforsoftdc/MainActivity.kt
+++ app/src/main/java/com/example/testforsoftdc/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.testforsoftdc
 
-import android.graphics.Typeface
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testforsoftdc.databinding.MainActivityBinding
@@ -12,7 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
-        setContentView(R.layout.main_activity)
+        val view = binding?.root
+        setContentView(view)
 
     }
 
